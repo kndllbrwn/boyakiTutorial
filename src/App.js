9c8e0687b3955ac17892ea0447c74afedb078f64
Kendall Brown <kndllbrwn1914@gmail.com>
2021-02-16 13:35:39 -0500

    initial commit reworked auth code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,205 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+// import { withAuthenticator } from '@aws-amplify/ui-react';
+
+import {
+  HashRouter,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import AllPosts from './containers/AllPosts';
+import PostsBySpecifiedUser from './containers/PostsBySpecifiedUser';
+
+// import Amplify from 'aws-amplify';
+// import PubSub from '@aws-amplify/pubsub'
+// import awsmobile from './aws-exports';
+
+// Amplify.configure(awsmobile);
+// PubSub.configure(awsmobile)
+
+import {Auth, Hub } from "aws-amplify"
+console.log(Auth);
+const initialFormState = {
+  username: "", password: "", email: "", authCode: "", formType: "signUp"
+}
+
+const drawerWidth = 240;
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#1EA1F2',
+      contrastText: "#fff",
+    },
+    background: {
+      default: '#15202B',
+      paper: '#15202B',
+    },
+    divider: '#37444C',
+  },
+  overrides: {
+    MuiButton: {
+      color: 'white',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Arial', 
+    ].join(','),
+  },
+  status: {
+    danger: 'orange',
+  },
+});
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    height: '100%',
+    width: 800,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+  const [formState, updateFormState] = useState(initialFormState)
+  const [user, updateUser] = useState(null)
+  async function checkUser(){
+    try {
+      debugger;
+      const user = await Auth.currentAuthenticatedUser()
+      updateUser(user)
+      updateFormState(() => ({...formState, formType: "signedIn"}))
+      console.log(user)
+    } catch {
+      updateUser(null)
+    }
+  }
+  async function setAuthListener(){
+    Hub.listen('auth', (data) => {
+      switch (data.payload.event) {
+        case 'signIn':
+            console.log('user signed in');
+            updateUser(data.username)
+            break;
+        case 'signOut':
+          console.log(("data from event: ", data))
+          updateFormState(() => ({...formState, formType: "signUp"}))
+            break;
+        default:
+          break
+    }});
+    
+  }
+  useEffect(() => {
+    checkUser()
+    setAuthListener()
+  }, [])
+  function onChange(e){
+    e.persist()
+    updateFormState(() => ({...formState, [e.target.name]: e.target.value}))
+  }
+  const {formType} = formState
+  async function signUp() {
+    const {username, email, password } = formState
+    await Auth.signUp({username, password, attributes: {email}})
+    updateFormState(() => ({...formState, formType: "confirmSignUp"}))
+  }
+  async function confirmSignUp() {
+    const {username, authCode} = formState
+    await Auth.confirmSignUp(username, authCode)
+    updateFormState(() => ({...formState, formType: "signIn"}))
+  }
+  async function signIn() {
+    const {username, password} = formState
+    await Auth.signIn(username, password)
+    updateFormState(() => ({...formState, formType: "signedIn"}))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root} >
+      {
+        formType === "signUp" && (
+          <div>
+            <input name="username" onChange={onChange} placeholder="username" />
+            <input name="password" type=" password" onChange={onChange} placeholder="password" />
+            <input name="email" onChange={onChange} placeholder="email" />
+            <button onClick={signUp}>Sign Up</button>
+            <button onClick={() => updateFormState(() => ({
+              ...formState, formType: "signIn"
+            }))}>Sign In</button>
+          </div>
+        )
+      }
+      {
+        formType === "signIn" && (
+          <div>
+            <input name="username" onChange={onChange} placeholder="username" />
+            <input name="password" type=" password" onChange={onChange} placeholder="password" />
+            <button onClick={signIn}>Sign In</button>
+          </div>
+        )
+      }
+      {
+        formType === "confirmSignUp" && (
+          <div>
+            <input name="authCode" type=" password" onChange={onChange} placeholder="Confirmation code" />
+            <button onClick={confirmSignUp}>Confirm Sign Up</button>
+          </div>
+        )
+      }
+      {/* {
+        formType === "signedIn" && (
+          <div>
+            <h1>Hello world, welcome user.</h1>
+            <button onClick={
+            () => Auth.signOut()
+            }>Sign Out</button>
+          </div>
+        )
+      } */}
+      {user && (<ThemeProvider theme={theme}>
+        <CssBaseline />
+        <HashRouter>
+          <Switch>
+            <Route exact path='/' component={AllPosts} />
+            <Route exact path='/global-timeline' component={AllPosts} />
+            <Route exact path='/:userId' component={PostsBySpecifiedUser}/>
+            <Redirect path="*" to="/" />
+          </Switch>
+        </HashRouter>
+      </ThemeProvider>)}
     </div>
   );
 }
 
+// export default withAuthenticator(App, {
+//   signUpConfig: {
+//     hiddenDefaults: ['phone_number']
+//   }
+// });
 export default App;
---
+++ src/containers/Sidebar.js
@@ -0,0 +1,172 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  TextField,
+  ListItemIcon,
+} from '@material-ui/core';
+import {
+  Person as PersonIcon,
+  Public as PublicIcon,
+} from '@material-ui/icons';
+
+import  { API, graphqlOperation } from 'aws-amplify';
+import {Auth} from 'aws-amplify';
+
+import { createPost } from '../graphql/mutations';
+import { useHistory } from 'react-router';
+
+console.log("a", Auth);
+const drawerWidth = 340;
+const MAX_POST_CONTENT_LENGTH = 140;
+
+const useStyles = makeStyles(theme => ({
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    position: 'relative',
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    position: 'relative',
+  },
+  toolbar: theme.mixins.toolbar,
+  textField: {
+    width: drawerWidth,
+  },
+  list: {
+    // overflowWrap: 'break-word',
+    width: 300,
+  },
+}));
+
+export default function Sidebar({activeListItem}) {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const [value, setValue] = React.useState('');
+  const [isError, setIsError] = React.useState(false);
+  const [helperText, setHelperText] = React.useState('');
+
+  const handleChange = event => {
+    setValue(event.target.value);
+    if (event.target.value.length > MAX_POST_CONTENT_LENGTH) {
+      setIsError(true);
+      setHelperText(MAX_POST_CONTENT_LENGTH - event.target.value.length);
+    } else {
+      setIsError(false);
+      setHelperText('');
+    }
+  };
+
+  const onPost = async () => {
+    const res = await API.graphql(graphqlOperation(createPost, { input: {
+      type: 'post',
+      content: value,
+      timestamp: Math.floor(Date.now() / 1000),
+    }})); 
+
+    console.log(res)
+    setValue('');
+  }
+
+//   async function signOut() {
+//     try {
+//       debugger
+//         await Auth.signOut();
+//     } catch (error) {
+//         console.log('error signing out: ', error);
+//     }
+// }
+
+  return (
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+      anchor="left"
+    >
+      <div className={classes.toolbar} />
+      <List>
+        <ListItem
+          button
+          selected={activeListItem === 'global-timeline'}
+          onClick={() => {
+            Auth.currentAuthenticatedUser().then((user) => {
+              history.push('/global-timeline');
+            })
+          }}
+          key='global-timeline'
+        >
+          <ListItemIcon>
+            <PublicIcon />
+          </ListItemIcon>
+          <ListItemText primary="Global Timeline" />
+        </ListItem>
+        <ListItem
+          button
+          selected={activeListItem === 'profile'}
+          onClick={() => {
+            Auth.currentAuthenticatedUser().then((user) => {
+              history.push('/' + user.username);
+            })
+          }}
+          key='profile'
+        >
+          <ListItemIcon>
+            <PersonIcon />
+          </ListItemIcon>
+          <ListItemText primary="Profile" />
+        </ListItem>
+        <ListItem key='post-input-field'>
+          <ListItemText primary={
+            <TextField
+              error={isError}
+              helperText={helperText}
+              id="post-input"
+              label="Type your post!"
+              multiline
+              rowsMax="8"
+              variant="filled"
+              value={value}
+              onChange={handleChange}
+              fullWidth
+              margin="normal"
+            />
+          } />
+        </ListItem>
+        <ListItem key='post-button'>
+          <ListItemText primary={
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={isError}
+              onClick={onPost}
+              fullWidth
+            >
+              Post
+            </Button>
+          } />
+        </ListItem>
+        <ListItem key='logout'>
+          <ListItemText primary={
+            <Button
+              variant="outlined"
+              onClick={() => Auth.signOut()}
+              fullWidth
+            >
+              Logout
+            </Button>
+          } />
+        </ListItem>
+      </List>
+    </Drawer>
+  )
+}
